532f5dc20684b4fac577896e32b5e3c84dd66668
SamiSha99 <sami.shakkour@hotmail.com>
2021-08-13 00:56:23 +0300

    Added website essentials

---
+++ scripts.js
@@ -0,0 +1,113 @@
+var canvas = document.getElementById("canvas-background");
+var ctx = canvas.getContext("2d");
+
+const lineLength = 400, maxSpeed = 15, minSpeed = 5;
+const maxDelayInterval = 150, minDelayInterval = 50; // in milliseconds
+var timer;
+var lines = [];
+
+function addNewLineValues(){
+
+    let line = {};
+    line.direction = GetDirection(Math.floor(Math.random() * 2));
+    line.offset = GetOffSet();
+    line.speed = (Math.random() * (15 - 5) + 5) * GetSpeedDirection(line.direction);
+    line.location = GetLocation(line.direction);
+    lines.push({
+        direction: line.direction,
+        offset: line.offset,
+        speed: line.speed,
+        location: line.location
+    });
+    timer = setTimeout(function(){addNewLineValues()}, Math.random() * (maxDelayInterval - minDelayInterval) + maxDelayInterval);
+}
+
+function drawLine() {
+    for(i = 0; i < lines.length; i++)
+    {
+        ctx.beginPath();
+        switch(lines[i].direction)
+        {
+            case "left":
+                ctx.moveTo(lines[i].location, lines[i].offset);
+                ctx.lineTo(lines[i].location + lineLength, lines[i].offset);
+                break;
+            case "right":
+                ctx.moveTo(lines[i].location + lineLength, lines[i].offset);
+                ctx.lineTo(lines[i].location, lines[i].offset);
+                break;
+        }
+        ctx.stroke();
+    }
+}
+
+function moveLine () {
+    for(i = 0; i < lines.length; i++)
+    {
+        lines[i].location += lines[i].speed;
+        if(shouldRemoveLine(i))
+        {
+            lines.splice(i,1);
+        }
+    }
+}
+
+function GetDirection(val){
+    
+    return val ? "left" : "right";
+}
+
+function GetOffSet(){
+    var maxLength = canvas.height;
+    var minLength = maxLength / 96;
+    return Math.floor(Math.random() * (maxLength - minLength) + minLength); 
+}
+
+function GetLocation(dir){
+    return dir == "left" ? (canvas.width + lineLength * 1.25) : -lineLength * 1.25;
+}
+
+function GetSpeedDirection(dir){    
+    return dir == "left" ? -1 : 1;
+}
+
+function shouldRemoveLine(i)
+{
+    let direction = lines[i].direction;
+    let location = lines[i].location;
+    if(direction == "left" && location < -lineLength) return true;
+    if(direction == "right" && location > canvas.width + lineLength) return true;
+    return false;
+}
+
+function loop() {
+    canvas.width = document.body.clientWidth;
+    //canvas.height = window.innerHeight;
+    canvas.height = window.innerHeight;
+    var grd = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    grd.addColorStop(0, "rgb(145, 53, 187)");
+    grd.addColorStop(0.5, "white");
+    grd.addColorStop(1, "rgb(145, 53, 187)");
+    ctx.strokeStyle = grd;
+    ctx.lineWidth = 7;
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    
+    moveLine();
+    drawLine();
+    requestAnimationFrame(loop);
+}
+
+window.onfocus = function() {
+    console.log('focus')
+    clearTimeout(timer);
+    addNewLineValues();
+};
+  
+window.onblur = function(){
+    console.log('blur')
+    clearTimeout(timer);
+};
+
+addNewLineValues();
+
+requestAnimationFrame(loop);
