b54e2d8efe2eecd0414a13bad68fdc01aeb4bf8f
SamiSha99 <sami.shakkour@hotmail.com>
2021-08-24 19:53:31 +0300

    improved the image showcase functionality

--- scripts/expandImage.js
+++ scripts/expandImage.js
@@ -1,9 +1,42 @@
-var lastCheckedRadio;
+imageMap = [
+    {image:"ice.jpg", text:"Hello this is my all new mod!"},
+    {image:"test.jpg", text:"A walk in the park"}
+];
 
-function handleZoom() {
-    if (document.querySelector('input[type="radio"]:checked') == lastCheckedRadio) {
-        lastCheckedRadio.checked = false;
-        lastCheckedRadio = undefined;
-    } else
-        lastCheckedRadio = document.querySelector('input[type="radio"]:checked');
+var divShowcase;
+var isShowcaseFadingOut;
+
+function showcaseImage(img) {
+    let str = img.getAttribute("src");
+    let imgName = str.substring(str.lastIndexOf("/") + 1, str.length);
+    divShowcase = document.createElement("div");
+    divShowcase.addEventListener("mousedown", removeImage, false);
+    divShowcase.id = "image-showcase";
+    divShowcase.classList.add("showcase-img");
+    divShowcase.innerHTML += '<img src="./img/' + imgName + '" />';
+    divShowcase.innerHTML += GetImageDescription(imgName);
+    document.body.insertBefore(divShowcase, document.body.firstChild);
+
+}
+
+function GetImageDescription(imgName) {
+    for(let i = 0; i < imageMap.length; i++) {
+        if(imgName != imageMap[i].image) continue;
+        return "<p>" + imageMap[i].text + "</p>";
+    }
+    return "<p><i>No description found.</i></p>";
+}
+
+function removeImage(event) {
+    if (divShowcase !== event.target || divShowcase == undefined || isShowcaseFadingOut) return;
+    divShowcase.classList.remove("showcase-img-fadein")
+    divShowcase.classList.add("showcase-img-fadeout");
+    isShowcaseFadingOut = true;
+    setTimeout(
+        function () {
+            divShowcase.innerHTML = '';
+            divShowcase.remove();
+            isShowcaseFadingOut = false;
+            divShowcase = undefined;
+        }, 550);
 }
