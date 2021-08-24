c03a3a6814fa6964e6b42de60231d580266f1f41
SamiSha99 <sami.shakkour@hotmail.com>
2021-08-25 02:36:42 +0300

    added scale animation

--- scripts/expandImage.js
+++ scripts/expandImage.js
@@ -4,7 +4,7 @@ imageMap = [
 ];
 
 var divShowcase;
-var isShowcaseFadingOut;
+var isShowcaseFadingOut, isShowcaseFadingIn;
 
 function showcaseImage(img) {
     let str = img.getAttribute("src");
@@ -13,24 +13,34 @@ function showcaseImage(img) {
     divShowcase.addEventListener("mousedown", removeImage, false);
     divShowcase.id = "image-showcase";
     divShowcase.classList.add("showcase-img");
-    divShowcase.innerHTML += '<img src="./img/' + imgName + '" />';
+    divShowcase.innerHTML += '<img class="showcase-img-scalein" id="shown-img" src="./img/' + imgName + '" />';
     divShowcase.innerHTML += GetImageDescription(imgName);
     document.body.insertBefore(divShowcase, document.body.firstChild);
-
+    isShowcaseFadingIn = true;
+    setTimeout(
+        function () {
+            isShowcaseFadingIn = false;
+        }, 375);
 }
 
 function GetImageDescription(imgName) {
     for(let i = 0; i < imageMap.length; i++) {
         if(imgName != imageMap[i].image) continue;
-        return "<p>" + imageMap[i].text + "</p>";
+        return '<p id="shown-description" class="showcase-img-scalein">' + imageMap[i].text + '</p>';
     }
-    return "<p><i>No description found.</i></p>";
+    return '<p id="shown-description" class="showcase-img-scalein"><i>No description found.</i></p>';
 }
 
 function removeImage(event) {
-    if (divShowcase !== event.target || divShowcase == undefined || isShowcaseFadingOut) return;
+    if (divShowcase !== event.target || divShowcase == undefined || isShowcaseFadingOut || isShowcaseFadingIn) return;
     divShowcase.classList.remove("showcase-img-fadein")
     divShowcase.classList.add("showcase-img-fadeout");
+    let img = document.getElementById("shown-img");
+    img.classList.remove("showcase-img-scalein");
+    img.classList.add("showcase-img-scaleout");
+    let desc = document.getElementById("shown-description");
+    desc.classList.remove("showcase-img-scalein");
+    desc.classList.add("showcase-img-scaleout");
     isShowcaseFadingOut = true;
     setTimeout(
         function () {
@@ -38,5 +48,5 @@ function removeImage(event) {
             divShowcase.remove();
             isShowcaseFadingOut = false;
             divShowcase = undefined;
-        }, 550);
+        }, 375);
 }
